f2f7a916f050d62e99f6319e9dd0162bb818b92f
digiT000 <fareldeksano000@gmail.com>
2024-06-24 14:37:44 +0700

    New Page Publish - blogr

--- blogr-landing-page-main/script.js
+++ blogr-landing-page-main/script.js
@@ -4,6 +4,10 @@
 let dropdownContainer = document.getElementsByClassName("dropDownNav");
 let dropdownAction = document.getElementsByClassName("btnDropdown");
 let btnIcon = document.getElementsByClassName("btnIcon");
+let btnMenu = document.getElementById("btnMenu");
+let menuMobile = document.getElementById("menuMobile");
+let closeMenu = document.getElementById("closeMenuMobile");
+let btnImg = document.getElementById("btnImg");
 
 //ASSIGN  Event listener ke button dropdown --> looping sabanyak class dropdownCotaniner
 for (let i = 0; i < dropdownAction.length; i++) {
@@ -13,8 +17,18 @@ for (let i = 0; i < dropdownAction.length; i++) {
     btnIcon[i].style.transform = " rotate(180deg)";
   });
 }
+// SHOW OR CLOSE MENU IN MOBILE
+btnMenu.addEventListener("click", function () {
+  if (!menuMobile.classList.contains("showMobileMenu")) {
+    menuMobile.classList.add("showMobileMenu");
+    changeImgIcon();
+  } else {
+    menuMobile.classList.remove("showMobileMenu");
+    changeImgIcon();
+  }
+});
 
-// Close the dropdown if the user clicks outside of it
+// Close the dropdown if the user clicks outside of the dropdown
 window.onclick = function (event) {
   console.log(event);
   if (
@@ -25,6 +39,16 @@ window.onclick = function (event) {
   }
 };
 
+// FUNCTION TO CHANGE IMAGE OF MENU IN MOBILE
+function changeImgIcon() {
+  if (menuMobile.classList.contains("showMobileMenu")) {
+    btnImg.src = "images/icon-close.svg";
+  } else {
+    btnImg.src = "images/icon-hamburger.svg";
+  }
+}
+
+//
 function closeDropDown() {
   for (let i = 0; i < dropdownContainer.length; i++) {
     var openDropdown = dropdownContainer[i];
